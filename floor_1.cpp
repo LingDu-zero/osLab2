16f0ea9fa142751bf81b9741e37c621c1cadf495
JiaxuanLiu <33046639+JiaxuanLiu@users.noreply.github.com>
2017-10-28 13:16:15 +0800

    Add files via upload

---
+++ floor_1.cpp
@@ -0,0 +1,15 @@
+#include"os2_new.h"
+
+int main()
+{
+	sharememoryget();
+	Direction floor_1;
+	int floor_now;
+	while (1)
+	{
+		printf("this is the direcion:\n");
+		putchar(floor_1);
+		printf("the is the floor number now: %d\t", floor_now);
+	}
+	return 0;
+}
---
+++ os2_new.h
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+enum Direction { down, up }; //确定方向
+int create(char *argc[])
+{
+	char *arg[] = { "ls","-l",NULL };
+	if (fork() == 0)
+	{
+		printf("execl...\n");
+		if (execl(argc, NULL)<0)
+		{
+			fprintf(stderr, "execl failed:%s", strerror(errno));
+			return -1;
+		}
+	}
+	printf("the process is over");
+	return 0;
+}
+
+void sharememoryget()
+{
+	shmget(1, 256, 0777);
+}
